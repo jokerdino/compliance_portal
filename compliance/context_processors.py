35ed0af22c37e99b9ad5f2eb2ca2cde3cb361c9a
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-09-28 22:45:32 +0530

    Added debug toolbar to paths

--- compliance/context_processors.py
+++ compliance/context_processors.py
@@ -1,24 +1,39 @@
+from django.db.models import Count, Q
 from django.utils.timezone import localdate
 from .models import Task
+
+
+# def tasks_count(request):
+#     """Returns the count of pending tasks globally."""
+#     today = localdate()
+#     due_today_count = Task.objects.filter(
+#         due_date=today, current_status="pending"
+#     ).count()
+
+#     overdue_count = Task.objects.filter(
+#         due_date__lt=today, current_status="pending"
+#     ).count()
+#     upcoming_count = Task.objects.filter(
+#         due_date__gt=today, current_status="pending"
+#     ).count()
+#     pending_count = Task.objects.filter(current_status="pending").count()
+#     return {
+#         "pending_tasks_count": pending_count,
+#         "due_today_count": due_today_count,
+#         "overdue_count": overdue_count,
+#         "upcoming_count": upcoming_count,
+#     }
 
 
 def tasks_count(request):
     """Returns the count of pending tasks globally."""
     today = localdate()
-    due_today_count = Task.objects.filter(
-        due_date=today, current_status="pending"
-    ).count()
 
-    overdue_count = Task.objects.filter(
-        due_date__lt=today, current_status="pending"
-    ).count()
-    upcoming_count = Task.objects.filter(
-        due_date__gt=today, current_status="pending"
-    ).count()
-    pending_count = Task.objects.filter(current_status="pending").count()
-    return {
-        "pending_tasks_count": pending_count,
-        "due_today_count": due_today_count,
-        "overdue_count": overdue_count,
-        "upcoming_count": upcoming_count,
-    }
+    counts = Task.objects.filter(current_status="pending").aggregate(
+        pending_tasks_count=Count("id"),
+        due_today_count=Count("id", filter=Q(due_date=today)),
+        overdue_count=Count("id", filter=Q(due_date__lt=today)),
+        upcoming_count=Count("id", filter=Q(due_date__gt=today)),
+    )
+
+    return counts
--- uicco/urls.py
+++ uicco/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import RedirectView
 
 from django.conf import settings
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -33,3 +34,4 @@ urlpatterns += [path("", RedirectView.as_view(url="compliance/", permanent=True)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += debug_toolbar_urls()
